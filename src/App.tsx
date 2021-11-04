4ef0f4ec965846881c68d5cbff7e8ef6e0f9f352
Emma <emma.mccracken14@gmail.com>
2021-11-04 14:54:27 +0000

    completed it

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+import { DogImage, DogItem } from "./components/DogItem";
 
 function App() {
+  const [imageArray, setImageArray] = useState<DogImage[]>([
+    { message: "", status: "" },
+  ]);
+
+  const handleGetDogImage = async () => {
+    const response = await fetch("https://dog.ceo/api/breeds/image/random");
+    const jsonBody: DogImage = await response.json();
+    setImageArray([...imageArray, jsonBody]);
+  };
+
+  const imageComponents = imageArray.map((DogImage) => {
+    return <DogItem DogImage={DogImage} />;
+  });
+
+  if (imageArray.length > 1) {
+    imageComponents.shift();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Dog Photo App Version 2</h1>
       </header>
+      <main>
+        <button onClick={handleGetDogImage}>Please show me another dog</button>{" "}
+        <br />
+        {imageArray.length > 1 && imageComponents}
+      </main>
     </div>
   );
 }
 
 export default App;
+
+// Junk code that didn't work:
+
+// async function initialiseState() {
+//   const initialResponse = await fetch(
+//     "https://dog.ceo/api/breeds/image/random"
+//   );
+//   const initialArray: DogImage[] = [await initialResponse.json()];
+//   const [imageArray, setImageArray] = useState<DogImage[]>(initialArray);
+// }
+
+// initialiseState();
+
+// const [imageArray, setImageArray] = useState<DogImage[]>(
+//   fetch("https://dog.ceo/api/breeds/image/random")
+//     .then((response) => {
+//       response.json();
+//     })
+//     .then((res) => res)
+// );
+//   if (imageArray.length > 1) {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <h1>Dog Photo App Version 2</h1>
+//         </header>
+//         <main>
+//           <button onClick={handleGetDogImage}>
+//             Please show me another dog
+//           </button>
+//           <div className="imageContainer">
+//             <img src={imageArray[1].message} alt="hi" />
+//           </div>
+//         </main>
+//       </div>
+//     );
+//   } else {
+//     return (
+
+//     );
+//   }
+// }
---
+++ src/components/DogItem.tsx
@@ -0,0 +1,16 @@
+export interface DogImage {
+  message: string;
+  status: string;
+}
+
+export interface DogItemProps {
+  DogImage: DogImage;
+}
+
+export function DogItem(props: DogItemProps): JSX.Element {
+  return (
+    <div className="imageContainer">
+      <img src={props.DogImage.message} alt="a dog" />
+    </div>
+  );
+}
